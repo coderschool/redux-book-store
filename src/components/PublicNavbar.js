d73c2778d1a306f201f08a0edda80f8fe0a7abbd
Your Name <joannersq@gmail.com>
2022-11-15 18:27:51 +0700

    solution

--- src/components/PublicNavbar.js
+++ src/components/PublicNavbar.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../images/logo.svg";
 import { NavLink } from "react-router-dom";
-import { AppBar, Box, IconButton, Typography, Menu, MenuItem, Button, Container, Toolbar } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-
-
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Button,
+  Container,
+  Toolbar,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useSelector } from "react-redux";
 
 const PublicNavbar = () => {
+  const { favorites } = useSelector((state) => state.bookstore);
   const [anchorElNav, setAnchorElNav] = useState(null);
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -15,13 +25,16 @@ const PublicNavbar = () => {
     setAnchorElNav(null);
   };
   return (
-    <AppBar position="static" sx={{ backgroundColor: "background.paper", color: "primary.darker" }}>
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "background.paper", color: "primary.darker" }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             <img src={logo} alt="CoderSchool" width="200px" />
           </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -35,68 +48,82 @@ const PublicNavbar = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
-
               <MenuItem
                 key="home"
                 onClick={handleCloseNavMenu}
-                component={NavLink} to="/"
+                component={NavLink}
+                to="/"
               >
                 <Typography textAlign="center">Home</Typography>
               </MenuItem>
               <MenuItem
                 key="reading"
                 onClick={handleCloseNavMenu}
-                component={NavLink} to="/reading"
+                component={NavLink}
+                to="/reading"
               >
                 <Typography textAlign="center">Reading List</Typography>
               </MenuItem>
-
             </Menu>
           </Box>
-          <Box
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <img src={logo} alt="CoderSchool" width="200px" />
           </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, alignItems: "center" }}>
-
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              alignItems: "center",
+            }}
+          >
             <Button
               key="home"
               onClick={handleCloseNavMenu}
-              component={NavLink} to="/"
+              component={NavLink}
+              to="/"
               variant="main"
-              sx={{ fontWeight: "600", marginTop: "auto", marginBottom: "auto", marginLeft: "1rem" }}
+              sx={{
+                fontWeight: "600",
+                marginTop: "auto",
+                marginBottom: "auto",
+                marginLeft: "1rem",
+              }}
             >
               Home
             </Button>
             <Button
               key="reading"
               onClick={handleCloseNavMenu}
-              component={NavLink} to="/reading"
+              component={NavLink}
+              to="/reading"
               variant="main"
-              sx={{ fontWeight: "600", marginTop: "auto", marginBottom: "auto", marginLeft: "1rem" }}
+              sx={{
+                fontWeight: "600",
+                marginTop: "auto",
+                marginBottom: "auto",
+                marginLeft: "1rem",
+              }}
             >
               Reading List
             </Button>
-
           </Box>
         </Toolbar>
-      </Container >
-    </AppBar >
+      </Container>
+    </AppBar>
   );
 };
 
